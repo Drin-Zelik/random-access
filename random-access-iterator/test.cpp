b20026a04230b8ecc081bb83a63dae2e9ba16132
Drin-Zelik <129373652+Drin-Zelik@users.noreply.github.com>
2023-12-25 10:02:38 +0300

    Update test.cpp

--- random-access-iterator/test.cpp
+++ random-access-iterator/test.cpp
@@ -6,29 +6,32 @@ TEST_CASE("can iterate") {
     tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
     int v = 1;
     for (int i : t) {
-        CHECK(i == v++);
+        CHECK(i == v);
+        v++;
     }
-    REQUIRE(v == 8);
+    REQUIRE(v == 7);
 }
 
 TEST_CASE("can reverse iterate") {
     tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
     int v = 7;
-    std::for_each(std::make_reverse_iterator(t.end()),
-                  std::make_reverse_iterator(t.begin()),
-                  [&v](int i) { CHECK(i == v--); });
-    REQUIRE(v == 0);
+    for (auto it = t.end(); it != t.begin(); it--) {
+        auto x = (*it);
+        CHECK(x == v--);
+    }
+    REQUIRE(v == 1);
 }
 
 TEST_CASE("operator[]") {
     tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
-    for (int i = 0; i < 7; ++i)
+    for (int i = 0; i < 7; ++i) {
         CHECK(t[i] == i + 1);
+    }
 }
 
 TEST_CASE("operator-") {
     tree t{{{1, 2, 3}, 4, {5, 6, {7, 8, {nil, 9, 10}}}}};
-    CHECK(t.end() - t.begin() == 10);
+    CHECK(t.end() - t.begin() == 9);
     CHECK(t.begin() + (t.end() - t.begin()) == t.end());
     for (auto i = t.begin(); i != t.end(); ++i)
         for (auto j = t.begin(); j != t.end(); ++j)
